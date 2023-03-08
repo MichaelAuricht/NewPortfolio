e0007ad54265ccc72475e6a4b5dcb888a6d4a7a1
Michael Auricht <michaelauricht@live.com.au>
2023-03-08 23:51:35 +1100

    removed mywork for now

--- client/src/utilities/commonUtis.js
+++ client/src/utilities/commonUtis.js
@@ -2,16 +2,11 @@ import Home from "../PortfolioContainer/Home/Home";
 import AboutMe from "../PortfolioContainer/AboutMe/AboutMe"
 import Resume from "../PortfolioContainer/Resume/Resume";
 import ContactMe from "../PortfolioContainer/ContactMe/ContactMe";
-import MyWork from "../PortfolioContainer/MyWork/MyWork";
 
 export const TOTAL_SCREENS = [
   {
     screen_name: "Home",
     component: Home,
-  },
-  {
-    screen_name: "My Work",
-    component: MyWork,
   },
   {
     screen_name: "About Me",
