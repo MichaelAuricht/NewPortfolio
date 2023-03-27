9cf6314ef366bf1fba5e614f2739dc9158c3723e
Michael Auricht <michaelauricht@live.com.au>
2023-03-28 10:09:22 +1100

    small changes to try to fix deploy

--- src/PortfolioContainer/MyWork/WorkCard.js
+++ src/PortfolioContainer/MyWork/WorkCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Batman from "../../images/batman.jpg";
-import { NavLink } from "react-router-dom";
 
 export default function WorkCard() {
   return (
@@ -8,17 +7,11 @@ export default function WorkCard() {
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
-          <img src={Batman} alt=""></img>
+          <img src="" alt=""></img>
           <h2 className="project-title">Project Title</h2>
           <div className="pro-details">
             <p>This is text</p>
             <div className="pro-btns">
-                <NavLink to="url.com" className="btn">
-                    View
-                </NavLink>
-                <NavLink to="url.com" className="btn">
-                    Source
-                </NavLink>
             </div>
           </div>
         </div>
