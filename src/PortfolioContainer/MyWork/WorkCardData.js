1959d00319c855dcabea88e39d23106812efe88c
Michael Auricht <michaelauricht@live.com.au>
2023-04-02 16:26:33 +1000

    added third project

--- src/PortfolioContainer/MyWork/WorkCardData.js
+++ src/PortfolioContainer/MyWork/WorkCardData.js
@@ -1,11 +1,12 @@
 import Batman from "../../images/batman.jpg";
 import QNA from "../../images/qna.jpg";
+import WDPlan from "../../images/workdayplanner.jpg"
 
 const ProjectCardData = [
   {
     imgsrc: Batman,
     title: "MovieBox",
-    text: "An app that allows the user to see reviews, trailers, ratings and related media of movies from the past, present and future. This was the first group project in my bootcamp.",
+    text: "An app that allows the user to see reviews, trailers, ratings and related media of movies from the past, present and future. This was the first group project in my bootcamp ending in August 2022.",
     view: "https://michaelauricht.github.io/project-group-1/",
     source: "https://github.com/MichaelAuricht/project-group-1"
   },
@@ -15,6 +16,13 @@ const ProjectCardData = [
     text: "A REACT App for asking questions which allows users to post answers as comments which can also be up and downvoted by other users to change their preference underneath the original post.",
     view: "https://ma-qna.herokuapp.com/",
     source: "https://github.com/MichaelAuricht/qna"
+  },
+  {
+    imgsrc: WDPlan,
+    title: "Work Day Planner",
+    text: "A simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.",
+    view: "https://michaelauricht.github.io/Work-Day-Planner/",
+    source: "https://github.com/MichaelAuricht/Work-Day-Planner"
   },
 ];
 
